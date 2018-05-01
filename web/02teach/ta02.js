cea582a6c2fc171a349ad4ad4bc3176a94e946f4
Jacob Alldredge <jakekalldredge@gmail.com>
2018-05-01 13:24:40 -0600

    finished ta02

---
+++ web/02teach/ta02.js
@@ -0,0 +1,8 @@
+function alert1() {
+  alert("Clicked!");
+}
+
+function applyClr() {
+  // alert("applyClr");
+  document.getElementById('d1').style.backgroundColor = document.getElementById('clrBox').value;
+}
