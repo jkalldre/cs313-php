1a47c4afa649c71ab9384852a7d0db45f625b04d
Jacob Alldredge <jakekalldredge@gmail.com>
2018-05-02 21:34:42 -0600

    working on homepage php

--- web/home_assigns.php
+++ web/home_assigns.php
@@ -9,8 +9,8 @@
   <body>
     <div class="nav">
       <ul class="nav">
-        <li class="active_a"><a href="">Home</a></li>
-        <li class="nav"><a href="home_assigns.php">Assignments</a></li>
+        <li class="nav"><a href="home.php">Home</a></li>
+        <li class="active_a"><a href="">Assignments</a></li>
         <li class="nav"><a href="https://www.w3schools.com/">W3Schools</a></li>
         <li class="nav"><a href="https://byui.brightspace.com/d2l/home/414715">Class Home</a></li>
         <li class="nav"><a id='timestamp'></a></li>
@@ -30,24 +30,6 @@
           });
         }
     </script>
-
-    <image class="img" src="img/me.jpg"/>
-    <div class="bio">
-    <p class="bio"> Hello, my name is Jacob Alldredge! I am a senior studying
-    Computer Science at Brigham Young University-Idaho.</p>
-    <p class="bio"> I have been married for
-    a little over one year and eight months. We do not have any kids yet, but we
-    have a little kitten named Archie who is just over a year old. </p>
-    <p class="bio">My favorite
-    hobbies all involve the outdoors, things like kayaking, fishing, and camping.
-    However, I am also a big fan of staying in and playing some videogame with friends
-    and family. This website will serve as my homepage for CS 313.</p>
-    <p class="bio">I served a two year mission in Recife, Brazil where I became
-    fluent in Portuguese. I absolutely fell in love with the food, culture, music,
-    and the people. I would give just about anything to spend another sunny day in that
-    costal city.</p>
-  </div>
-
 
     <ul class="scheme">
       <li class="clrs" id="c1">Electric</li>
--- web/php/timestamp.php
+++ web/php/timestamp.php
@@ -1,4 +1,4 @@
 <?php
 date_default_timezone_set('America/Boise');
-    echo $timestamp = date('H:i:s');
+    echo $timestamp = date('h:i:s');
 ?>
