7a4d184d434802e4817cd87c27ed23dd123df98d
Jacob Alldredge <jakekalldredge@gmail.com>
2018-05-22 19:59:15 -0600

    working on prove05

--- web/index.php
+++ web/index.php
@@ -1,72 +1,72 @@
 <?php
-  session_start();
-  // unset($_POST['login']);
-  $dbtest = true;
-  if($dbtest && !isset($_POST['login'])) {
-    $dbUrl = getenv('DATABASE_URL');
-    $dbopts = parse_url($dbUrl);
-    $dbHost = $dbopts["host"];
-    $dbPort = $dbopts["port"];
-    $dbUser = $dbopts["user"];
-    $dbPassword = $dbopts["pass"];
-    $dbName = ltrim($dbopts["path"],'/');
-    alert("Synced");
-    try
-    {
-      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    }
-    catch (PDOException $ex)
-    {
-      echo 'Error!: ' . $ex->getMessage();
-      die();
-    }
+session_start();
+// unset($_POST['login']);
+$dbtest = true;
+if($dbtest && !isset($_POST['login'])) {
+  $dbUrl = getenv('DATABASE_URL');
+  $dbopts = parse_url($dbUrl);
+  $dbHost = $dbopts["host"];
+  $dbPort = $dbopts["port"];
+  $dbUser = $dbopts["user"];
+  $dbPassword = $dbopts["pass"];
+  $dbName = ltrim($dbopts["path"],'/');
+  alert("Synced");
+  try
+  {
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
   }
-  alert("made it past dbconnect!");
-  // include('./php/dbconnect.php');
-  $pwd = 'supergoodpassword';
-  function alert($msg) {
-    echo "<script type='text/javascript'>alert('$msg');</script>";
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
   }
-  // alert("wrong answer");
-  if(isset($_POST['login'])){
-    // alert($_POST['usrpwd'] == $pwd);
-    // alert($_POST['usrpwd']);
-    alert("Running Query");
-    if($dbtest){
-      $dbquery = "SELECT password from public.user WHERE username='{$_POST['usrname']}';"
-      alert($dbquery);
-      $pwquery = $db->query("SELECT password from public.user WHERE username='{$_POST['usrname']}';");
+}
+alert("made it past dbconnect!");
+// include('./php/dbconnect.php');
+$pwd = 'supergoodpassword';
+function alert($msg) {
+  echo "<script type='text/javascript'>alert('$msg');</script>";
+}
+// alert("wrong answer");
+if(isset($_POST['login'])){
+  // alert($_POST['usrpwd'] == $pwd);
+  // alert($_POST['usrpwd']);
+  alert("Running Query");
+  if($dbtest){
+    $dbquery = "SELECT password from public.user WHERE username='{$_POST['usrname']}';";
+    alert($dbquery);
+    $pwquery = $db->query("SELECT password from public.user WHERE username='{$_POST['usrname']}';");
     alert("Executing");
     $pwquery->execute();
     alert("Executed");
     alert($pwquery);}
   }
   ?>
-<!-- <!doctype html> -->
-<html>
-<head>
-  <title>Task Manager</title>
-  <link rel="stylesheet" href="css/project.css">
-  <script src="js/project.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-</head>
-<body>
-  <div class="header">Task Manager - Login in</div>
-  <form method="post" action="">
-  <div class="imgsignin">
-    <img src="img/task.png">
-  </div>
+  <!-- <!doctype html> -->
+  <html>
+  <head>
+    <title>Task Manager</title>
+    <link rel="stylesheet" href="css/project.css">
+    <script src="js/project.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="header">Task Manager - Login in</div>
+    <form method="post" action="">
+      <div class="imgsignin">
+        <img src="img/task.png">
+      </div>
 
-  <div class="signin">
-  <lable for="usrname"><b>Username:</b></lable>
-  <input class="signin" type="text" placeholder="Enter Username" name="usrname" required>
-  <br />
+      <div class="signin">
+        <lable for="usrname"><b>Username:</b></lable>
+        <input class="signin" type="text" placeholder="Enter Username" name="usrname" required>
+        <br />
 
-  <lable for="usrpwd"><b>Password:</b></lable>
-  <input class="signin" type="password" placeholder="Enter Password" name="usrpwd" required>
+        <lable for="usrpwd"><b>Password:</b></lable>
+        <input class="signin" type="password" placeholder="Enter Password" name="usrpwd" required>
 
-  <button class="loginbtn" type="submit" name="login">Login</button>
-</div>
-</form>
-</body>
-</html>
+        <button class="loginbtn" type="submit" name="login">Login</button>
+      </div>
+    </form>
+  </body>
+  </html>
