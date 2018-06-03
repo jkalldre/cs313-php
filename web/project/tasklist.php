047898b4bc34ee29a39582eec41b76887fc8818e
Jacob Alldredge <jakekalldredge@gmail.com>
2018-06-02 20:08:01 -0600

    progress on prove06

--- web/project/tasklist.php
+++ web/project/tasklist.php
@@ -102,6 +102,7 @@ if ($_POST['newt'] == 'process'){
   $title = $_POST['title'];
   $category = ucwords(strtolower($_POST['category']));
   $date = $_POST['date'];
+  echo $date;
   if (!existingCategory($category)){
     insertCategory($category);
     $categories = getCategories();
