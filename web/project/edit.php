0fbee16ab6f11a6f048b6aada7cd8625f12415eb
Jacob Alldredge <jakekalldredge@gmail.com>
2018-06-02 19:25:50 -0600

    progress on prove06

--- web/project/edit.php
+++ web/project/edit.php
@@ -112,7 +112,7 @@ if (isset($_POST['newt1'])){
     }
     echo " Category: $category\nTitle: $title\nTask-ID: $taskid\n";
     $db->exec([$title,$categories[$category]['title'],$taskid]);
-    header('location:tasklist.php?user='.$user);
+    // header('location:tasklist.php?user='.$user);
   } catch (PDOException $e){
     $e->getMessage();
     echo $e;
