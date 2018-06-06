70adf91037caa6f803e56c4004c21d12b642e9c5
Jacob Alldredge <jakekalldredge@gmail.com>
2018-06-06 08:14:07 -0600

    Cleaning project

--- web/project/newuser.php
+++ web/project/newuser.php
@@ -1,24 +1,26 @@
 <?php
 session_start();
 $dbtest = true;
-if($dbtest) {
-  $dbUrl = getenv('DATABASE_URL');
-  $dbopts = parse_url($dbUrl);
-  $dbHost = $dbopts["host"];
-  $dbPort = $dbopts["port"];
-  $dbUser = $dbopts["user"];
-  $dbPassword = $dbopts["pass"];
-  $dbName = ltrim($dbopts["path"],'/');
-  try
-  {
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-  }
-  catch (PDOException $ex)
-  {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
-  }
-}
+require('../php/dbconnect.php');
+
+// if($dbtest) {
+//   $dbUrl = getenv('DATABASE_URL');
+//   $dbopts = parse_url($dbUrl);
+//   $dbHost = $dbopts["host"];
+//   $dbPort = $dbopts["port"];
+//   $dbUser = $dbopts["user"];
+//   $dbPassword = $dbopts["pass"];
+//   $dbName = ltrim($dbopts["path"],'/');
+//   try
+//   {
+//     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+//   }
+//   catch (PDOException $ex)
+//   {
+//     echo 'Error!: ' . $ex->getMessage();
+//     die();
+//   }
+// }
 if(isset($_POST['newuser'])){
   // if(!isset($db)) alert("DB IS NOT SET");
   // else alert("db is set");
